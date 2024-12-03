a05ce4749a8cb5fef4db8b9cb03a693be39fd521
Pierre.Jacobs <pierre.jacobs@dyna-mo.com>
2024-12-03 10:39:28 +0100

    add input data

---
+++ advent-day-1/target/debug/deps/advent_day_1.d
@@ -0,0 +1,5 @@
+C:\Users\Pierre.Jacobs\LearningAndDevelopment\AdventCode\advent-of-code\advent-day-1\target\debug\deps\advent_day_1.exe: src/main.rs
+
+C:\Users\Pierre.Jacobs\LearningAndDevelopment\AdventCode\advent-of-code\advent-day-1\target\debug\deps\advent_day_1.d: src/main.rs
+
+src/main.rs:
