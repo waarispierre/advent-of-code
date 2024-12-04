cdd6129ca2af9522500d26f4d5a372bbdcdf8b1c
Pierre.Jacobs <pierre.jacobs@dyna-mo.com>
2024-12-04 14:26:50 +0100

    day 2 part 2

--- advent-day-2/src/main.rs
+++ advent-day-2/src/main.rs
@@ -9,21 +9,42 @@ fn main() -> std::io::Result<()> {
 fn read_list() -> std::io::Result<()> {
     let input_data = fs::read_to_string("src/input_data/input.txt")?;
 
-    let mut _safe_reports = 0;
+    let mut _safe_reports_without_dampening = 0;
+    let mut _safe_reports_with_dampening = 0;
+
     for line in input_data.lines() {
         let columns: Vec<i32> = line
             .split_whitespace()
             .map(|s| s.parse::<i32>().expect("Invalid number"))
             .collect();
 
-        let rule_one: bool = satisfies_rule_one(columns.clone())?;
-        let rule_two: bool = satisfies_rule_two(columns.clone())?;
-        if rule_one && rule_two {
-            _safe_reports += 1;
+        if satisfies_all_rules(columns.clone())? {
+            _safe_reports_without_dampening += 1;
+        } else {
+            match report_dampening(columns.clone()) {
+                Ok(updated_report) => {
+                    if satisfies_rule_one(updated_report.clone())?
+                        && satisfies_rule_two(updated_report.clone())?
+                    {
+                        _safe_reports_with_dampening += 1;
+                    }
+                }
+                Err(_) => {}
+            }
         }
     }
 
-    println!("Number of safe reports: {}", _safe_reports);
+    let safe_reports = _safe_reports_with_dampening + _safe_reports_without_dampening;
+
+    println!(
+        "Number of safe reports without dampening: {}",
+        _safe_reports_without_dampening
+    );
+    println!(
+        "Number of safe reports with dampening: {}",
+        _safe_reports_with_dampening
+    );
+    println!("Number of safe reports: {}", safe_reports);
     Ok(())
 }
 
@@ -46,7 +67,6 @@ fn satisfies_rule_one(data: Vec<i32>) -> std::io::Result<bool> {
 }
 
 fn satisfies_rule_two(data: Vec<i32>) -> std::io::Result<bool> {
-    let mut _count = 0;
     let mut rule_satisfied: bool = false;
     for i in 0..data.len() - 1 {
         let difference = data[i] - data[i + 1];
@@ -60,4 +80,25 @@ fn satisfies_rule_two(data: Vec<i32>) -> std::io::Result<bool> {
     }
 
     return Ok(rule_satisfied);
+}
+
+fn report_dampening(data: Vec<i32>) -> std::io::Result<Vec<i32>> {
+    for i in 0..data.len() {
+        let mut test_data = data.clone();
+
+        test_data.remove(i);
+
+        if satisfies_all_rules(test_data.clone())? {
+            return Ok(test_data);
+        }
+    }
+
+    return Err(std::io::Error::new(
+        std::io::ErrorKind::InvalidData,
+        "could not dampen report",
+    ));
+}
+
+fn satisfies_all_rules(data: Vec<i32>) -> std::io::Result<bool> {
+    return Ok(satisfies_rule_one(data.clone())? && satisfies_rule_two(data.clone())?);
 }
