3783143518fb209347ea0a62a38454ecc5288555
Pierre.Jacobs <pierre.jacobs@dyna-mo.com>
2024-12-04 14:53:48 +0100

    day 2 part 2 - cleanup

--- advent-day-2/src/main.rs
+++ advent-day-2/src/main.rs
@@ -1,38 +1,10 @@
 use std::fs;
 
 fn main() -> std::io::Result<()> {
-    println!("Starting checks");
-    read_list()?;
-    Ok(())
-}
+    let data = read_list()?;
 
-fn read_list() -> std::io::Result<()> {
-    let input_data = fs::read_to_string("src/input_data/input.txt")?;
-
-    let mut _safe_reports_without_dampening = 0;
-    let mut _safe_reports_with_dampening = 0;
-
-    for line in input_data.lines() {
-        let columns: Vec<i32> = line
-            .split_whitespace()
-            .map(|s| s.parse::<i32>().expect("Invalid number"))
-            .collect();
-
-        if satisfies_all_rules(columns.clone())? {
-            _safe_reports_without_dampening += 1;
-        } else {
-            match report_dampening(columns.clone()) {
-                Ok(updated_report) => {
-                    if satisfies_rule_one(updated_report.clone())?
-                        && satisfies_rule_two(updated_report.clone())?
-                    {
-                        _safe_reports_with_dampening += 1;
-                    }
-                }
-                Err(_) => {}
-            }
-        }
-    }
+    let (_safe_reports_without_dampening, updated_report_data) = part_one(data.clone())?;
+    let mut _safe_reports_with_dampening = part_two(updated_report_data.clone())?;
 
     let safe_reports = _safe_reports_with_dampening + _safe_reports_without_dampening;
 
@@ -45,7 +17,64 @@ fn read_list() -> std::io::Result<()> {
         _safe_reports_with_dampening
     );
     println!("Number of safe reports: {}", safe_reports);
+
     Ok(())
+}
+
+fn part_one(data: Vec<Vec<i32>>) -> std::io::Result<(i32, Vec<Vec<i32>>)> {
+    let mut _safe_reports = 0;
+    let mut indexes_to_remove: Vec<usize> = Vec::new();
+    let mut updated_report_data = data.clone();
+
+    let mut _count = 0;
+    for report in data.iter() {
+        if satisfies_all_rules(report.clone())? {
+            _safe_reports += 1;
+            indexes_to_remove.push(_count);
+        }
+        _count += 1;
+    }
+
+    indexes_to_remove.sort_by(|a, b| b.cmp(a));
+
+    for remove_index in indexes_to_remove.iter() {
+        updated_report_data.remove(*remove_index);
+    }
+    return Ok((_safe_reports, updated_report_data));
+}
+
+fn part_two(data: Vec<Vec<i32>>) -> std::io::Result<i32> {
+    let mut _safe_reports = 0;
+
+    for report in data.iter() {
+        match report_dampening(report.clone()) {
+            Ok(updated_report) => {
+                if satisfies_rule_one(updated_report.clone())?
+                    && satisfies_rule_two(updated_report.clone())?
+                {
+                    _safe_reports += 1;
+                }
+            }
+            Err(_) => {}
+        }
+    }
+
+    return Ok(_safe_reports);
+}
+
+fn read_list() -> std::io::Result<Vec<Vec<i32>>> {
+    let input_data = fs::read_to_string("src/input_data/input.txt")?;
+    let mut all_data: Vec<Vec<i32>> = Vec::new();
+
+    for line in input_data.lines() {
+        let columns: Vec<i32> = line
+            .split_whitespace()
+            .map(|s| s.parse::<i32>().expect("Invalid number"))
+            .collect();
+
+        all_data.push(columns);
+    }
+    return Ok(all_data);
 }
 
 fn satisfies_rule_one(data: Vec<i32>) -> std::io::Result<bool> {
